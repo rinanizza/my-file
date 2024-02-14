761b771300463180932c57122348988b3ef475d0
https://github.com/rinanizza <rinanizza77@gmail.com>
2024-02-14 21:20:21 +0100

    first commit

---
+++ portfolio.js
@@ -0,0 +1,4 @@
+document.getElementById("h1").style.color = "blue";
+document.getElementById("h2").style.color = "blue";
+document.getElementById("h2-1").style.color = "blue";
+document.getElementById("h2-2").style.color = "blue";
